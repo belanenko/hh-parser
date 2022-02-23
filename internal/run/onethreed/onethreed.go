ab9e711427693412b5ecfcccc42bc4b5afdb4569
belanenko <belanenkotimofey@gmail.com>
2022-02-23 20:41:32 +0300

    Revert "add one thread app and few updates"
    This reverts commit d25b349d50ff29d5ee3032f7f9d20a5be32b8cd6.

--- internal/run/onethreed/onethreed.go
+++ internal/run/onethreed/onethreed.go
@@ -2,48 +2,44 @@ package onethreed
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/hh-parser/internal/models/proxy"
 	"github.com/hh-parser/internal/models/vacancy"
 	"github.com/hh-parser/internal/requests/hh"
 	"github.com/hh-parser/internal/storages/proxystorage"
-	"github.com/hh-parser/internal/storages/vacancystorage"
 )
 
-func Run(startIndex, countPages int) {
+func Run() {
 	this_proxyStorage := proxystorage.GetStorage()
 	this_proxyStorage.Add(
 		proxy.Proxy{
 			Schema: proxy.HTTP,
-			Host:   "p.webshare.io",
-			Port:   80,
+			Host:   "209.127.191.180",
+			Port:   9279,
 			UserInfo: proxy.UserInfo{
-				Username: "untrxaih-rotate",
+				Username: "untrxaih",
 				Password: "kmff7s4ojnto",
 			},
 		},
 	)
 
-	this_vacancystorage := vacancystorage.Storage
+	// this_vacancystorage := vacancystorage.Storage
 	baseUrl := `https://hh.ru/vacancy/`
-
-	stopIndex := startIndex + countPages
+	startIndex := 50000050
+	stopIndex := startIndex + 15
 
 	for thisIndex := startIndex; thisIndex < stopIndex; thisIndex++ {
 		var newVacancy vacancy.Vacancy
 		this_url := fmt.Sprintf("%s%d", baseUrl, thisIndex)
-		statuscode, err := hh.GetVacancy(this_url, this_proxyStorage.GetFormatedProxy(), &newVacancy)
-		if err != nil {
-			log.Println(err)
-			continue
+		hh.GetVacancy(this_url, this_proxyStorage.GetFormatedProxy(), &newVacancy)
+		switch {
+		case newVacancy.BaseSalary.Value.MaxValue != 0 && newVacancy.BaseSalary.Value.MinValue != 0:
+			fmt.Printf("Название вакансии: %s, Зарплата от: %d до %d \n", newVacancy.Title, newVacancy.BaseSalary.Value.MinValue, newVacancy.BaseSalary.Value.MaxValue)
+		case newVacancy.BaseSalary.Value.MaxValue == 0 && newVacancy.BaseSalary.Value.MinValue != 0:
+			fmt.Printf("Название вакансии: %s, Зарплата от: %d  \n", newVacancy.Title, newVacancy.BaseSalary.Value.MinValue)
+		case newVacancy.BaseSalary.Value.MaxValue != 0 && newVacancy.BaseSalary.Value.MinValue == 0:
+			fmt.Printf("Название вакансии: %s, Зарплата до: %d  \n", newVacancy.Title, newVacancy.BaseSalary.Value.MaxValue)
 		}
-		if statuscode != 200 {
-			log.Printf("status code is not 200, code: %d", statuscode)
-			continue
-		}
-
-		this_vacancystorage.Pop(newVacancy)
 	}
 
 }
