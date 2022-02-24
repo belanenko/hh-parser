9ec14fae0ba9e06bbf0972b2bc4e39e770d7f9cc
Timofey Belanenko <BelanenkoTimofey@gmail.com>
2022-02-24 15:15:58 +0300

    read proxy from textfile

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -2,30 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/hh-parser/internal/models/proxy"
 	"github.com/hh-parser/internal/run/onethreed"
 	"github.com/hh-parser/internal/storages/proxystorage"
 	"github.com/hh-parser/internal/storages/vacancystorage"
+	"github.com/hh-parser/internal/utli/filereader"
+	"github.com/hh-parser/internal/utli/formater"
 )
 
 func main() {
 	this_proxyStorage := proxystorage.GetStorage()
-	this_proxyStorage.Add(
-		proxy.Proxy{
-			Schema: proxy.HTTP,
-			Host:   "p.webshare.io",
-			Port:   80,
-			UserInfo: proxy.UserInfo{
-				Username: "untrxaih-rotate",
-				Password: "kmff7s4ojnto",
-			},
-		},
-	)
+	strProxies := filereader.ReadAllLines("/home/tim/code/github.com/belanenko/hh-parser/assets/socks5.txt")
 
+	for _, p := range strProxies {
+		if p == "" {
+			continue
+		}
+		this_proxyStorage.Add(formater.StringToProxy(proxy.SOCKS5, p)...)
+	}
+
+	log.Printf("Was loaded %d proxies in storage", len(this_proxyStorage.Proxies))
 	this_vacancystorage := vacancystorage.Storage
 
-	onethreed.RunOneThreed(50000000, 3, this_vacancystorage, this_proxyStorage)
+	onethreed.RunOneThreed(50000000, 100, this_vacancystorage, this_proxyStorage)
 	fmt.Println(this_vacancystorage.Len())
 
 	for _, v := range this_vacancystorage.PushCount(5) {
