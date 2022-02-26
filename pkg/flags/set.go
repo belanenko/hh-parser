460eceac90c0e973bbca3452f6cbc277b3aefe37
Timofey Belanenko <BelanenkoTimofey@gmail.com>
2022-02-26 10:55:01 +0300

    Changed flag set method

--- pkg/flags/set.go
+++ pkg/flags/set.go
@@ -2,12 +2,24 @@ package flags
 
 import "flag"
 
-func Set() (*int, *string, *int, *int) {
+type Config struct {
+	NumGorutines  int
+	ProxyFilePath string
+	IndexStart    int
+	Count         int
+}
+
+func Set() *Config {
 	numGorutines := flag.Int("threads", 1, "count of threads")
 	proxyFilePath := flag.String("pfp", "", "proxy file path")
 	indexStart := flag.Int("startid", 0, "Start parsing index")
 	indexCount := flag.Int("countid", 1, "Count vacancy to parsing")
 	flag.Parse()
 
-	return numGorutines, proxyFilePath, indexStart, indexCount
+	return &Config{
+		NumGorutines:  *numGorutines,
+		ProxyFilePath: *proxyFilePath,
+		IndexStart:    *indexStart,
+		Count:         *indexCount,
+	}
 }
