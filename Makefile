285549a51859ea775a15c3a629fd44da4a8b4cde
Timofey Belanenko <BelanenkoTimofey@gmail.com>
2022-02-26 14:09:28 +0300

    add dbconfig flag

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 run:
 	clear 
-	go run ./cmd/app/app.go --threads 5 --pfp=/home/tim/code/github.com/belanenko/hh-parser/assets/socks5.txt --startid=1000 --countid=50
+	go run ./cmd/app/app.go --threads 5 --pfp=/home/tim/code/github.com/belanenko/hh-parser/assets/socks5.txt --startid=1000 --countid=50 --clickconf /home/tim/.config/hh-parser/config.json
 test: 
 	clear
 	go test -v ./... -timeout 30s
+.PHONY:build
+build:
+	clear 
+	go build ./cmd/app/app.go
+
 DEFAULT_GOAL=run
--- pkg/flags/set.go
+++ pkg/flags/set.go
@@ -1,12 +1,15 @@
 package flags
 
-import "flag"
+import (
+	"flag"
+)
 
 type Config struct {
-	NumGorutines  int
-	ProxyFilePath string
-	IndexStart    int
-	Count         int
+	NumGorutines    int
+	ProxyFilePath   string
+	IndexStart      int
+	Count           int
+	ClickConfigPath string
 }
 
 func Set() *Config {
@@ -14,12 +17,15 @@ func Set() *Config {
 	proxyFilePath := flag.String("pfp", "", "proxy file path")
 	indexStart := flag.Int("startid", 0, "Start parsing index")
 	indexCount := flag.Int("countid", 1, "Count vacancy to parsing")
+	clickConfigPath := flag.String("clickconf", "", "clickhouse *.json configuration file path")
+
 	flag.Parse()
 
 	return &Config{
-		NumGorutines:  *numGorutines,
-		ProxyFilePath: *proxyFilePath,
-		IndexStart:    *indexStart,
-		Count:         *indexCount,
+		NumGorutines:    *numGorutines,
+		ProxyFilePath:   *proxyFilePath,
+		IndexStart:      *indexStart,
+		Count:           *indexCount,
+		ClickConfigPath: *clickConfigPath,
 	}
 }
